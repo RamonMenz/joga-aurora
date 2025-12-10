25075d5b425e45ed0e024bb09ff5aaac88204b30
Ramon Pedro Menz <ramonpedro.rs@gmail.com>
2025-12-09 23:52:04 -0300

    setando exposed headers

--- src/main/java/br/feevale/security/config/CorsConfig.java
+++ src/main/java/br/feevale/security/config/CorsConfig.java
@@ -3,6 +3,7 @@ package br.feevale.security.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -23,11 +24,11 @@ public class CorsConfig {
                 .toList();
 
         final var corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.setAllowedOriginPatterns(List.of("*"));
         corsConfiguration.setAllowedOrigins(origins);
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of(HttpHeaders.CONTENT_DISPOSITION));
 
         final var urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
